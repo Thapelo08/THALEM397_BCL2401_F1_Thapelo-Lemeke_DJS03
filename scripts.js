07557b09eb27499ce2bcd4fe7597f55d5f63409d
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-05-17 09:36:31 +0200

    initiallize the criteria

--- scripts.js
+++ scripts.js
@@ -163,7 +163,7 @@ const setupEventListeners = () => {
         }
     });
 };
-//
+// initialize the criteria
 document.addEventListener('DOMContentLoaded', () => {
     initializePage();
     setupEventListeners();
